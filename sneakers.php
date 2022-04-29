acae4e8eca55a97f1fb441fd436bba73ec924c98
DariaRadu2001 <dariaradu20@yahoo.com>
2022-04-29 05:47:23 +0300

    nu

--- sneakers.php
+++ sneakers.php
@@ -1,7 +1,55 @@
 <?php
-$mysqli = new mysqli("localhost", "root", "", "sneakers");
+session_start();
+require_once("dbcontroller.php");
+$db_handle = new DBController();
+if(!empty($_GET["action"])) {
+    switch($_GET["action"]) {
+        case "add":
+            if(!empty($_POST["quantity"])) {
+                $productByCode = $db_handle->runQuery("SELECT * FROM sneakers WHERE s_id='" . $_GET["s_id"] . "'");
+                $itemArray = array($productByCode[0]["s_id"]=>array('name'=>$productByCode[0]["name"], 'marime'=>$productByCode[0]["marime"], 'quantity'=>$_POST["quantity"], 'price'=>$productByCode[0]["price"], 'image'=>$productByCode[0]["image"]));
 
+                if(!empty($_SESSION["cart_item"])) {
+                    if(in_array($productByCode[0]["s_id"],array_keys($_SESSION["cart_item"]))) {
+                        foreach($_SESSION["cart_item"] as $k => $v) {
+                            if($productByCode[0]["s_id"] == $k) {
+                                if(empty($_SESSION["cart_item"][$k]["quantity"])) {
+                                    $_SESSION["cart_item"][$k]["quantity"] = 0;
+                                }
+                                $_SESSION["cart_item"][$k]["quantity"] += $_POST["quantity"];
+                            }
+                        }
+                    } else {
+                        $_SESSION["cart_item"] = array_merge($_SESSION["cart_item"],$itemArray);
+                    }
+                } else {
+                    $_SESSION["cart_item"] = $itemArray;
+                }
+            }
+            break;
+        case "remove":
+            if(!empty($_SESSION["cart_item"]))
+            {
+                foreach($_SESSION["cart_item"] as $k => $v)
+                {
+                    if($_GET["s_id"] == $k)
+                        unset($_SESSION["cart_item"][$k]);
+                    if(empty($_SESSION["cart_item"]))
+                        unset($_SESSION["cart_item"]);
+                }
+            }
+            break;
 
+        case "empty":
+            unset($_SESSION["cart_item"]);
+            break;
+
+        case "buy":
+            unset($_SESSION["cart_item"]);
+            header("Location: buy.html");
+            break;
+    }
+}
 ?>
 
 <!DOCTYPE html>
@@ -31,6 +79,7 @@ $mysqli = new mysqli("localhost", "root", "", "sneakers");
     <div class="txt-heading">Shopping Cart</div>
 
     <a id="btnEmpty" href="sneakers.php?action=empty">Empty Cart</a>
+    <a id="btnEmpty" href="sneakers.php?action=buy">Buy</a>
     <?php
     if(isset($_SESSION["cart_item"]))
     {
@@ -40,7 +89,7 @@ $mysqli = new mysqli("localhost", "root", "", "sneakers");
         <table class="tbl-cart" cellpadding="10" cellspacing="1">
             <tr>
                 <th style="text-align:left;">Name</th>
-                <th style="text-align:left;">Code</th>
+                <th style="text-align:left;">Size</th>
                 <th style="text-align:right;" width="5%">Quantity</th>
                 <th style="text-align:right;" width="10%">Unit Price</th>
                 <th style="text-align:right;" width="10%">Price</th>
@@ -51,12 +100,12 @@ $mysqli = new mysqli("localhost", "root", "", "sneakers");
                 $item_price = $item["quantity"]*$item["price"];
                 ?>
                 <tr>
-                    <td><img src="<?php echo $item["image"]; ?>" class="cart-item-image" /><?php echo $item["name"]; ?></td>
-                    <td><?php echo $item["code"]; ?></td>
+                    <td><img src="<?php echo "poze/".$item["image"]; ?>" class="cart-item-image" /><?php echo $item["name"]; ?></td>
+                    <td><?php echo $item["marime"]; ?></td>
                     <td style="text-align:right;"><?php echo $item["quantity"]; ?></td>
                     <td  style="text-align:right;"><?php echo "$ ".$item["price"]; ?></td>
                     <td  style="text-align:right;"><?php echo "$ ". number_format($item_price,2); ?></td>
-                    <td style="text-align:center;"><a href="sneakers.php?action=remove&code=<?php echo $item["code"]; ?>" class="btnRemoveAction"><img src="poze/icon-delete.png" alt="Remove Item" /></a></td>
+                    <td style="text-align:center;"><a href="sneakers.php?action=remove&s_id=<?php echo $item["s_id"]; ?>><img src="poze/icon-delete.png" alt="Remove Item" /></a></td>
                 </tr>
                 <?php
                 $total_quantity += $item["quantity"];
@@ -82,92 +131,26 @@ $mysqli = new mysqli("localhost", "root", "", "sneakers");
 </div>
 
 
-
-<br><br><br>
-    <table style="width:100%">
-        <tr>
-            <th>
-                <a href="s1.html">
-                    <img src="poze/s1.jpg" alt="s1" height=300px width=400px style="object-fit: contain">
-                </a>
-                <p>Nike Air Force 1 '07 PRM "Pastel Reveal" sneakers</p>
-                <form action="/action_page.php">
-                    <label for="marime"><div class="p2">Size:</div></label>
-                    <select name="marime" id="marime">
-                        <option value="35">35</option>
-                        <option value="36">36</option>
-                        <option value="40">40</option>
-                        <option value="41">41</option>
-                    </select>
+<div id="product-grid">
+    <div class="txt-heading">Products</div>
+    <?php
+    $product_array = $db_handle->runQuery("SELECT * FROM sneakers ORDER BY s_id ASC");
+    if (!empty($product_array)) {
+        foreach($product_array as $key=>$value){
+            ?>
+            <div class="product-item">
+                <form method="post" action="sneakers.php?action=add&s_id=<?php echo $product_array[$key]["s_id"]; ?>">
+                    <div class="product-image"><img src="<?php echo "poze/".$product_array[$key]["image"]; ?>"></div>
+                    <div class="product-tile-footer">
+                        <div class="product-title"><?php echo $product_array[$key]["name"]; ?></div>
+                        <div class="product-price"><?php echo "PRICE: ".$product_array[$key]["price"]."LEI"; ?></div>
+                        <?php echo "<br>SIZE: ".$product_array[$key]["marime"]; ?>
+                        <div class="cart-action"><input type="text" class="product-quantity" name="quantity" value="1" size="2" /><input type="submit" value="Add to Cart" class="btnAddAction" /></div>
+                    </div>
                 </form>
-            </th>
-
-            <td>
-                <a href="s2.html">
-                    <img src="poze/s2.jpg" alt="s2" height=300px width=400px style="object-fit: contain">
-                </a>
-                <p>Travis Scott x Air Jordan 1 Low 'Mocha'</p>
-                <form action="/action_page.php">
-                    <label for="marime"><div class="p2">Size:</div></label>
-                    <select name="marime" id="marime">
-                        <option value="40">40</option>
-                        <option value="41">41</option>
-                    </select>
-                </form>
-            </td>
-        </tr>
-
-        <tr>
-            <th>
-                <a href="s3.html">
-                    <img src="poze/s3.jpg" alt="s3" height=300px width=400px style="object-fit: cover">
-                </a>
-                <p>Jordan 1 Retro High Court Purple White</p>
-                <form action="/action_page.php">
-                    <label for="marime"><div class="p2">Size:</div></label>
-                    <select name="marime" id="marime">
-                        <option value="39">39</option>
-                        <option value="40">40</option>
-                        <option value="45">45</option>
-                    </select>
-                </form>
-            </th>
-
-            <td>
-                <a href="s4.html">
-                    <img src="poze/s4.jpg" alt="s4" height=300px width=420px style="object-fit: cover">
-                </a>
-                <p>NIKE DUNK LOW Skateboard Sneakers</p>
-                <form action="/action_page.php">
-                    <label for="marime"><div class="p2">Size:</div></label>
-                    <select name="marime" id="marime">
-                        <option value="36">36</option>
-                        <option value="37">37</option>
-
-                    </select>
-                </form>
-            </td>
-        </tr>
-
-        <tr>
-            <th>
-                <a href="s5.html">
-                    <img src="poze/s5.jpg" alt="s5" height=300px width=400px style="object-fit: cover">
-                </a>
-                <p>Comme Des Garçons Play x Converse</p>
-                <form action="/action_page.php">
-                    <label for="marime"><div class="p2">Size:</div></label>
-                    <select name="marime" id="marime">
-                        <option value="36">36</option>
-                        <option value="37">37</option>
-                        <option value="41">41</option>
-                    </select>
-                </form>
-
-            </th>
-
-        </tr>
-
-    </table>
-</div></body>
-</html>
+            </div>
+            <?php
+        }
+    }
+    ?>
+</div>
